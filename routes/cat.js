a7d25995b0826da062184e7776e178666179b993
brennangw <brennangoffwallace@gmail.com>
2014-06-25 23:37:20 -0500

    worked on quering for catagories

--- routes/cat.js
+++ routes/cat.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 var db = require('database').db;
 
+
 /* GET users listing. */
+
+//this still isnt working have a question on SO about it though.
+
 router.get('/:cat', function(req, res) {
-    console.log(typeof req.params.cat);
-    var cursor = db.Item.find({ cats: { $elemMatch: req.params.cat}});
-    cursor.map( function(myDoc) {console.log("myDoc.name: " + myDoc.name)});
+    db.Item.find({"cats": {"$in": req.params.cat}}, function (err, cats) {
+        if (err) return console.error(err);
+        console.log(JSON.stringify(cats, undefiend, 4));
+    });
 
 });
 
